bacae5e50c85e0c9abcf10fb9909391592a8c0b7
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2023-01-06 12:05:20 -0500

    lots of changes - essentially another init commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
-function App() {
+function App() { 
 
-  const [data, setData] = useState();  
-  const [location, setLocation] = useState(''); 
-  const [lat, setLat] = useState(0);
-  const [lon, setLong] = useState(0);
+  // const [data, setData] = useState();  
+  // const [location, setLocation] = useState(''); 
+  // const [lat, setLat] = useState(0);
+  // const [lon, setLong] = useState(0);
 
-  const geoCodingAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=1b8f10e6dec639a10401c3ffbc2a56c3`
-  //const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=1b8f10e6dec639a10401c3ffbc2a56c3`;
-  const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&type=accurate&appid=1b8f10e6dec639a10401c3ffbc2a56c3&cnt=5`; 
-
-
-
+  const [weatherData, setWeatherData] = useState([]); 
+  const [city, setCity] = useState('');
+  const [error, setError] = useState();
+  
 
   
-  const searchLocation = (event) => {  
-    if(event.key === 'Enter') {
-    axios.get(geoCodingAPI).then((res) => {
-      const geoData = res.data[0]; 
-      // setData(geoData);
-      // console.log(geoData); 
+  async function getWeatherData() {
+    try {
+      setError();
+      setWeatherData([]);
+      // const geoCodingAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=1b8f10e6dec639a10401c3ffbc2a56c3`
+      //const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=1b8f10e6dec639a10401c3ffbc2a56c3`;
+      // const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&type=accurate&appid=1b8f10e6dec639a10401c3ffbc2a56c3&cnt=5`; 
+
+      //get long & lat based on city the user inputs
+      let response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=1b8f10e6dec639a10401c3ffbc2a56c3`);
+      const lat = response.data[0].lat;
+      const long = response.data[0].lon;
+
+      // make weather api call
+      const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&cnt=5&appid=https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&cnt=5&appid=88bad94d4996fa813e2a40d794f02155`);
+      setWeatherData(weatherData.data.list);
+    } catch(e) {
+      console.log(e)
+      setError(e);
+    }
+  }
+
+  
+//   const searchLocation = (event) => {  
+//     if(event.key === 'Enter') {
+//     axios.get(geoCodingAPI).then((res) => {
+//       setData(res.data);
+//       console.log(res.data); 
       
-      setLat(geoData.lat); 
-      setLong(geoData.lon); 
-      console.log('Lat is: ' + lat);
-      console.log('long is: ' + lon);
-    }); 
+//     }); 
   
-  };
-}
+//   };
+// }
 
 
 
@@ -46,12 +62,15 @@ return (
     <div className="search-container">
         <h1>My Weather App</h1>
         <div className='search'>
-          <input 
+          {/* <input 
             value={location}
             onChange={event => setLocation(event.target.value)}
             onKeyPress={searchLocation}
             placeholder="Enter a location"
-            type="text"/>
+            type="text"/> */}
+            <input
+              onChange={event => setCity(event.target.value)} />
+            <button onClick={getWeatherData}>Search</button>
         </div>
     </div>
 
@@ -61,6 +80,12 @@ return (
         <p>{data.city.name}</p> 
       </div>
     )} */}
+
+    {weatherData.map((setWeatherData, index) => 
+      <div className="weather-container">
+        Current Weather: {weatherData.main.temp}
+      </div>
+    )}
   </div>
 
 
