8a84ffb4a3921ca3ddab94ff36e8c4b61efe9131
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2023-01-08 13:39:08 -0500

    api returns all data. styling is started.

--- src/App.js
+++ src/App.js
@@ -1,100 +1,62 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
+import './index.css'; 
+
 
 function App() { 
-
-  // const [data, setData] = useState();  
-  // const [location, setLocation] = useState(''); 
-  // const [lat, setLat] = useState(0);
-  // const [lon, setLong] = useState(0);
-
+  // set the state for the weather data and the city
   const [weatherData, setWeatherData] = useState([]); 
   const [city, setCity] = useState('');
-  const [error, setError] = useState();
-  
+  const weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&type=accurate&APPID=1b8f10e6dec639a10401c3ffbc2a56c3&cnt=5`; 
 
-  
-  async function getWeatherData() {
-    try {
-      setError();
-      setWeatherData([]);
-      // const geoCodingAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=1b8f10e6dec639a10401c3ffbc2a56c3`
-      //const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=1b8f10e6dec639a10401c3ffbc2a56c3`;
-      // const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&type=accurate&appid=1b8f10e6dec639a10401c3ffbc2a56c3&cnt=5`; 
-
-      //get long & lat based on city the user inputs
-      let response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=1b8f10e6dec639a10401c3ffbc2a56c3`);
-      const lat = response.data[0].lat;
-      const long = response.data[0].lon;
-
-      // make weather api call
-      const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&cnt=5&appid=https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&cnt=5&appid=88bad94d4996fa813e2a40d794f02155`);
-      setWeatherData(weatherData.data.list);
-    } catch(e) {
-      console.log(e)
-      setError(e);
+  // search the city and get the weather data
+  const searchCity = (event) => { 
+    if(event.key === 'Enter' || event.key === 'Get Weather') {
+      axios.get(weatherAPI).then((response) => {
+        setWeatherData(response.data.list); 
+        console.log(response.data.list);
+        for (let i = 0; i < response.data.list.length; i++) {
+          console.log(response.data.list[i].dt_txt);
+          console.log(response.data.list[i].main.temp);
+          console.log(response.data.list[i].main.humidity);
+        }
+        for (let i = 0; i < response.data.list.length; i++) {
+          console.log(response.data.list[i].weather[0].description);
+        }
+      });
     }
   }
 
-  
-//   const searchLocation = (event) => {  
-//     if(event.key === 'Enter') {
-//     axios.get(geoCodingAPI).then((res) => {
-//       setData(res.data);
-//       console.log(res.data); 
-      
-//     }); 
-  
-//   };
-// }
-
-
-
-// const geocodeLocation = (event) => {
-
-
-// }
-
-return ( 
-  <div className="app">
-
-    {/* SEARCH COMPONENT */}
-    <div className="search-container">
-        <h1>My Weather App</h1>
-        <div className='search'>
-          {/* <input 
-            value={location}
-            onChange={event => setLocation(event.target.value)}
-            onKeyPress={searchLocation}
-            placeholder="Enter a location"
-            type="text"/> */}
-            <input
-              onChange={event => setCity(event.target.value)} />
-            <button onClick={getWeatherData}>Search</button>
+  return (
+    <div className="app">
+      <div className="headerBar">
+        <div className="header">
+          <h1>My Weather App</h1>
         </div>
+          <div className="search">  
+            <input
+              value={city}
+              onChange={event => setCity(event.target.value)}
+              onKeyPress={searchCity}
+              placeholder="Enter a city"
+              type="text"/>  
+            <button onClick={searchCity}>Get Weather</button>
+          </div>
+        <div className="date">
+        </div>
+        <div className="city">
+        </div>
+        <div className="description"> 
+        </div>
+        <div className="minTemp">
+        </div>
+        <div className="maxTemp">
+        <div className="humidity">
+        </div>
+        </div>
+      </div>
     </div>
-
-    {/* RESULTS COMPONENT */}
-    {/* {data && (
-      <div className="location">
-        <p>{data.city.name}</p> 
-      </div>
-    )} */}
-
-    {weatherData.map((setWeatherData, index) => 
-      <div className="weather-container">
-        Current Weather: {weatherData.main.temp}
-      </div>
-    )}
-  </div>
-
-
   );
 }
 
-
-
 export default App;
-
-
-
