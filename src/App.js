460c4208c58e59d1a0211fe191390fa0699d81b5
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2023-01-08 14:24:14 -0500

    Separated components, need to add styling and make sure functionality is intact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 import './index.css'; 
 
 
+
 function App() { 
   // set the state for the weather data and the city
-  const [weatherData, setWeatherData] = useState([]); 
+  const [weatherData, setWeatherData] = useState([]);
   const [city, setCity] = useState('');
   const weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&type=accurate&APPID=1b8f10e6dec639a10401c3ffbc2a56c3&cnt=5`; 
 
@@ -21,40 +23,76 @@ function App() {
           console.log(response.data.list[i].main.humidity);
         }
         for (let i = 0; i < response.data.list.length; i++) {
-          console.log(response.data.list[i].weather[0].description);
+          console.log(response.data.list[i].weather[0].description) 
         }
       });
     }
   }
 
-  return (
-    <div className="app">
-      <div className="headerBar">
-        <div className="header">
-          <h1>My Weather App</h1>
-        </div>
-          <div className="search">  
-            <input
-              value={city}
-              onChange={event => setCity(event.target.value)}
-              onKeyPress={searchCity}
-              placeholder="Enter a city"
-              type="text"/>  
-            <button onClick={searchCity}>Get Weather</button>
+  class HeaderBar extends React.Component {
+    render() {
+      return (
+        <div className="HeaderBar">
+          <div className="header">
+            <h1>My Weather App</h1>
           </div>
-        <div className="date">
+          <div className="search">
+            <input
+              value={this.props.city}
+              onChange={event => this.props.setCity(event.target.value)}
+              onKeyPress={this.props.searchCity}
+              placeholder="Enter a city"
+              type="text"/>
+            <button onClick={this.props.searchCity}>Get Weather</button>
+          </div>
         </div>
-        <div className="city">
-        </div>
-        <div className="description"> 
-        </div>
-        <div className="minTemp">
-        </div>
-        <div className="maxTemp">
-        <div className="humidity">
-        </div>
-        </div>
-      </div>
+      );
+    }
+  }
+  
+  class Results extends React.Component {
+       render() {
+       return (
+            <div className="Results">
+            <div className="date">
+            </div>
+            <div className="city">
+            </div>
+            <div className="description">
+            </div>
+            <div className="minTemp">
+            </div>
+            <div className="maxTemp">
+            <div className="humidity">
+            </div>
+            </div>
+            </div>
+       );
+       }
+       }
+  
+  class EntireAppPage extends React.Component {
+       render() {
+       return (
+            <div className="EntireAppPage">
+            <HeaderBar
+                 city={this.props.city}
+                 setCity={this.props.setCity}
+                 searchCity={this.props.searchCity}
+            />
+            <Results />
+            </div>
+       );
+        }
+  }
+
+  return (
+    <div className="App">
+      <EntireAppPage
+        city={city}
+        setCity={setCity}
+        searchCity={searchCity}
+      />
     </div>
   );
 }
